0d04effedd6bd03b85098423556c8278cb13ea8d
dFionov <dmitryfionov@gmail.com>
2017-10-11 21:41:22 +0300

    fixed up to todays changes

--- Algorithms/Warmup/solve_me_first.rs
+++ Algorithms/Warmup/solve_me_first.rs
@@ -1,13 +1,24 @@
+// https://www.hackerrank.com/challenges/solve-me-first
+
+
 use std::io;
 use std::io::prelude::*;
 
 fn main() {
 
-	let stdin = io::stdin();
+    let stdin = io::stdin(); // representation of stdin
 
-	let a: i32 = stdin.lock().lines().next().unwrap().unwrap().trim().parse().unwrap();
-	let b: i32 = stdin.lock().lines().next().unwrap().unwrap().trim().parse().unwrap();
+    let a: i32 = stdin.lock()                                    // locked version of stdin
+                      .lines()                                   // iterator over lines in stdin
+                      .next()                                    // Next line. Since line is Option<Result<String>> we need to check:
+                      .expect("stdin is empty")                  // existance 
+                      .expect("error reading line from stdin")   // and errorless
+                      .trim()                                    // remove leading and trailing whitespace
+                      .parse()                                   // try to parse value to i32
+                      .expect("can't parse i32");                // and check if its ok
 
-	println!("{}", a + b);
+    let b: i32 = stdin.lock().lines().next().unwrap().unwrap().trim().parse().unwrap(); //same as a, but instead of expect we use unwrap for shortness
+
+    println!("{}", a + b);
 
 }
